1816fcdf94d2b6f61a21d289ac49c3117a3721b4
Jake Correa <jcorrea257@gmail.com>
2015-10-15 03:46:07 -0400

    Start twilio quickstart

---
+++ lib/sms_test.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'yaml'
+
+# Moving configuration settings to a separate hash available throughout the application
+SETTINGS = YAML.load(File.read(File.expand_path('../../config/settings.yml', __FILE__)))
+
+begin
+  @client = Twilio::REST::Client.new SETTINGS['twilio_sid'], SETTINGS['twilio_auth_token']
+
+  message = @client.account.messages.create(
+    :body => "This is a test of the In The Pocket Emergency Broadcast system...",
+    :to => "7147463354",
+    :from => "2673172682"
+  )
+rescue Twilio::REST::RequestError => e
+  puts e.message
+end
